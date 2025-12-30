3a8600e28212c6806f3786a72bbfaf7c7912d8fd
NIRMALYA RANJAN SARKAR <nirmalyaranjansarkar@gmail.com>
2025-12-30 15:29:31 +0530

    feat: Update dependencies and Vite configuration
    Updates external script dependencies and enables environment variable loading in Vite. This ensures the application uses the latest versions of libraries like React, React DOM, and Firebase, and allows for dynamic configuration through environment variables.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -7,14 +7,22 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, __dirname, '');
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
     },
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './'),
+      },
+    },
+    define: {
+      'process.env': env
+    }
+  }
 })
